526a9c9c5696ab7a93c9b01fefe5cf244402c00e
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-03-31 21:39:34 -0600

    added update

---
+++ script.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+let particlesArrary = []
+
+class Particle {
+  constructor(x, y){
+    this.x = x
+    this.y = y
+    this.size = 10
+    this.weight = 2
+    this.directionX = 1
+  }
+  update(){
+    this.weight += 0.01
+    this.y += this.weight
+    
+  }
+}
